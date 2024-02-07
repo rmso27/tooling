c7c93df771361470b29f54e0881d7b95ee2805b1
rmso27 <rubenmsoliveira@gmail.com>
2024-02-07 09:36:14 +0000

    added 'apt update' to update keys

--- toolkit-0.0.1/kubectl/install.sh
+++ toolkit-0.0.1/kubectl/install.sh
@@ -12,6 +12,7 @@ sudo apt update
 # add kubectl key and repo
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
 
 # install kubectl
 sudo apt install -y kubectl
